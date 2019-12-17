6b7feaee2b0b7a49f93156163c1538cdf4e24402
Afrim Azizi <afrim.azizi@duna.mk>
2019-12-17 15:50:30 +0100

    update

--- src/app/a-file-previewer/a-file-previewer.component.ts
+++ src/app/a-file-previewer/a-file-previewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, SimpleChanges, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, OnChanges, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'aFilePreviewer',
@@ -9,51 +9,54 @@ export class AFilePreviewerComponent implements OnChanges {
 
   // VARIABLES
   @Input('filepath') FilePath: string;
+  CurrentPath: string;
   FileType: string;
   HtmlPreview: string;
   RetryCount: number = 0;
 
   constructor() { }
 
-  ngOnChanges() {
-    this.showPreview();
+  ngOnChanges(changes: SimpleChanges) {
+    const currentChange = changes['FilePath'].currentValue;
+    if (currentChange !== undefined && currentChange !== null && currentChange !== '' && currentChange !== '{{pdfName}}' && (currentChange.includes('http://') || currentChange.includes('https://'))) {
+      this.CurrentPath = currentChange;
+      this.showPreview();
+    } else {
+      this.FileType = 'None';
+      this.HtmlPreview = `<span> Unsupported file preview or file cannot be loaded. </span>`;
+    }
   }
 
   showPreview() {
-    // console.group('DataAnalytics');
-    // console.log('FilePath: ' + this.FilePath);
-    // console.log('Count: ' + this.RetryCount);
-    // console.log('Type: ' + this.FileType);
-    // console.groupEnd();
-    if (this.FilePath !== null && this.FilePath !== '' && (this.FilePath.includes('http://') || this.FilePath.includes('https://'))) {
+    if (this.CurrentPath !== null && this.CurrentPath !== '' && (this.CurrentPath.includes('http://') || this.CurrentPath.includes('https://'))) {
       const fileExtension = this.FilePath.split('.');
       if (fileExtension.length > 0) {
         this.FileType = fileExtension[fileExtension.length - 1];
-      }
 
-      switch (this.FileType) {
-        case 'pdf':
-          this.HtmlPreview = `<embed src="${this.FilePath}" width="100%" height="500" alt="pdf"
+        switch (this.FileType) {
+          case 'pdf':
+            this.HtmlPreview = `<embed src="${this.CurrentPath}" width="100%" height="500" alt="pdf"
       pluginspage="http://www.adobe.com/products/acrobat/readstep2.html">`;
-          break;
+            break;
 
-        case 'png':
-          this.HtmlPreview = `<img src="${this.FilePath}" width="100%" alt="FileType" />`;
-          break;
+          case 'png':
+            this.HtmlPreview = `<img src="${this.CurrentPath}" width="100%" alt="FileType" />`;
+            break;
 
-        case 'jpg':
-          this.HtmlPreview = `<img src="${this.FilePath}" width="100%" alt="FileType" />`;
-          break;
+          case 'jpg':
+            this.HtmlPreview = `<img src="${this.CurrentPath}" width="100%" alt="FileType" />`;
+            break;
 
-        case 'gif':
-          this.HtmlPreview = `<img src="${this.FilePath}" width="100%" alt="FileType" />`;
-          break;
+          case 'gif':
+            this.HtmlPreview = `<img src="${this.CurrentPath}" width="100%" alt="FileType" />`;
+            break;
 
-        default:
-          this.HtmlPreview = `<span> Unsupported file preview, Click <a href="${this.FilePath}"> here </a> to download! </span>`;
-          break;
+          default:
+            this.HtmlPreview = `<span> Unsupported file preview, Click <a href="${this.CurrentPath}"> here </a> to download! </span>`;
+            break;
+        }
       }
-    } else if ((this.FilePath === null || this.FilePath === '') && this.RetryCount === 3) {
+    } else if ((this.CurrentPath === null || this.CurrentPath === '') && this.RetryCount === 3) {
       this.FileType = 'None';
       this.HtmlPreview = `<span> Unsupported file preview or file cannot be loaded. </span>`;
     } else {
